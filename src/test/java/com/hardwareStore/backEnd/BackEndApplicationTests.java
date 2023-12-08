a50426c005f4dbe121ef41902b607b0721a96d1b
lazoescobar <agustin_lazo_e@hotmail.com>
2023-12-08 16:06:08 -0300

    creacion de proyecto

---
+++ src/test/java/com/hardwareStore/backEnd/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardwareStore.backEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
